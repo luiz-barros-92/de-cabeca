03d5b3dc4f6fb000d0c796cea02269efdebb09e7
Luiz Barros <lck.barros@gmail.com>
2025-01-20 23:05:25 -0300

    Adicionada calculadora de porcentagem

--- src/script.js
+++ src/script.js
@@ -1,3 +1,10 @@
 window.onload = function() { 
     document.getElementById("ano").innerHTML = new Date().getFullYear(); 
-} 
+}
+
+function calcularPorc1() {
+    const valor1 = parseFloat(document.getElementById('porc1').value);
+    const valor2 = parseFloat(document.getElementById('porc2').value);
+    const resultado = (valor1 / 100) * valor2;
+    document.getElementById('outPorc1').value = `${resultado.toFixed(2).replace('.', ',')}`;
+  }
